18ec7ea7d438b15b22e127521a0f053f689bbd2f
kevin <kevinmamaniuba@gmail.com>
2023-11-13 19:06:08 -0300

    Commit de tomar foto y mandarla al servidor

--- src/take-photos.py
+++ src/take-photos.py
@@ -1,5 +1,8 @@
 from picamera import PiCamera
 from time import sleep
+import requests
 camera = PiCamera()
 for i in range(10):
        camera.capture(f"./foto{i}.jpg") 
+       files = {'file': open(f'./foto{i}.jpg', 'rb')}
+       print(requests.post(url='http://10.9.121.15:5000/uploader', files=files))
