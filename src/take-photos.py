a97d5aa6a608bee2c470e5050338a95d3f7eebb6
kevin <kevinmamaniuba@gmail.com>
2023-11-14 19:47:36 -0300

    codigo de sensor con camara

--- src/take-photos.py
+++ src/take-photos.py
@@ -1,8 +1,22 @@
 from picamera import PiCamera
-from time import sleep
+import time
+import RPi.GPIO as GPIO
 import requests
+
 camera = PiCamera()
-for i in range(10):
-       camera.capture(f"./foto{i}.jpg") 
-       files = {'file': open(f'./foto{i}.jpg', 'rb')}
-       print(requests.post(url='http://10.9.121.15:5000/uploader', files=files))
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+PIR_PIN = 23
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+print('iniciando el modulo sensor PIR (clickea parar para salir)')
+print ('Listo')
+
+while True:
+  if GPIO.input(PIR_PIN):
+    print('Movimiento detectado')
+    camera.capture(f"./foto.jpg") 
+    files = {'file': open(f'./foto.jpg', 'rb')}
+    print(requests.post(url='http://10.9.121.208:5000/uploader', files=files))  
+  time.sleep(1)
